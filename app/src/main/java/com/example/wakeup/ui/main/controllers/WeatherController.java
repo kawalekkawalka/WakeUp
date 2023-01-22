2a4d3845f54ae5d58a9048a40b704d6990388fe2
Hubert <hubertkawalko@gmail.com>
2023-01-23 00:55:53 +0100

    connection to weather api

--- app/src/main/java/com/example/wakeup/ui/main/controllers/WeatherController.java
+++ app/src/main/java/com/example/wakeup/ui/main/controllers/WeatherController.java
@@ -2,13 +2,18 @@ package com.example.wakeup.ui.main.controllers;
 
 import static com.example.wakeup.ui.main.utils.Utils.getWeatherFromApi;
 
+
+import android.util.Log;
+
 import java.io.IOException;
+import java.util.List;
 
 
 public class WeatherController {
     public static String getWeather(String type, String Location) throws IOException {
-        String[] conditions= getWeatherFromApi();
-        System.out.println(conditions);
+        Log.d("Retrofit", "Weszlo w controller");
+        System.out.println(getWeatherFromApi());
+        System.out.println("HHHHHHHHHHHHHHHHHHHHHH");
         return "";
     }
 }
--- app/src/main/java/com/example/wakeup/ui/main/utils/Utils.java
+++ app/src/main/java/com/example/wakeup/ui/main/utils/Utils.java
@@ -1,16 +1,27 @@
 package com.example.wakeup.ui.main.utils;
 
+import android.os.AsyncTask;
+import android.util.Log;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Utils {
 
@@ -38,31 +49,45 @@ public class Utils {
     }
 
 
-    public static String[] getWeatherFromApi() throws IOException {
-        String API_URL = "https://api.open-meteo.com/v1/forecast?latitude=53.13&longitude=23.16&hourly=temperature_2m,relativehumidity_2m,weathercode,surface_pressure";
-        String temperature;
-        String humidity;
-        String weatherCode;
-        String surfacePressure;
+    public static List getWeatherFromApi() throws IOException {
+        List conditions = new ArrayList<String>();
 
-        URL url = new URL(API_URL);
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-        //InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-        InputStreamReader in = new InputStreamReader(urlConnection.getInputStream());
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.open-meteo.com/v1/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-//            // convert the input stream to a string
-//            String responseString = readStream(in);
-//            // parse the string as a JSON object
-//            Gson gson = new Gson();
-//            JsonObject json = gson.fromJson(responseString, JsonObject.class);
-//            // access the data in the JSON object
-//            temperature = json.get("temperature_2m").getAsString();
-//            humidity = json.get("relativehumidity_2m").getAsString();
-//            weatherCode = json.get("weathercode").getAsString();
-//            surfacePressure = json.get("surface_pressure").getAsString();
+        WeatherApi weatherApi = retrofit.create(WeatherApi.class);
 
-        urlConnection.disconnect();
-        String[] conditions = {"temperature, humidity, weatherCode, surfacePressure"};
+        List<String> hourly = Arrays.asList("temperature_2m", "relativehumidity_2m", "weathercode", "surface_pressure");
+        Call<JsonObject> call = weatherApi.getWeatherData(53.13, 23.16, hourly);
+        Log.d("Retrofit", call.request().url().toString());
+        call.enqueue(new Callback<JsonObject>() {
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                if (response.isSuccessful()) {
+                    // Parse the JSON response and extract the temperature and humidity values
+                    Log.d("Retrofit", "Polaczylo z api");
+                    JsonObject json = response.body();
+                    String weatherCode = json.get("timezone").getAsString();
+                    Log.d("Retrofit", weatherCode);
+//                    String temperature = json.get("temperature_2m").getAsString();
+//                    String humidity = json.get("relativehumidity_2m").getAsString();
+//                    String surfacePressure = json.get("surface_pressure").getAsString();
+//                    Collections.addAll(conditions,temperature, humidity, weatherCode, surfacePressure);
+                }
+                else {
+                    Log.d("Retrofit", "Nie polaczylo z api :(");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+                Log.e("Retrofit", "Error connecting to API", t);
+            }
+        });
         return conditions;
     }
+
+
 }
